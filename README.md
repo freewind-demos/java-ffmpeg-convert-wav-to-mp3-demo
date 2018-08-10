76476f22868370486f7a2c9e2245f62af117e676
freewind <nowindlee@gmail.com>
2018-08-10 18:14:52 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-Java Hello World Demo
-=====================
+Java Convert wav to mp3 Demo
+============================
+
+Use [ffmpeg wrapper](https://github.com/bramp/ffmpeg-cli-wrapper) to convert wav file to mp3.
+
+```
+brew install ffmpeg
+```
+
+Check the path of `ffmpeg`:
+
+```
+$ which ffmpeg
+/usr/local/bin/ffmpeg
+```
+
+Modify the path in `Hello.java` if not same.
 
 Run `Hello.java` in your IDE.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,28 @@
 package demo;
 
-public class Hello {
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.File;
+import java.io.IOException;
+
+public class Hello {
+    
+    private static String FFMPEG_PATH = "/usr/local/bin/ffmpeg";
+    private static File output = new File("data/demo.mp3");
+
+    public static void main(String[] args) throws IOException {
+        FFmpeg ffmpeg = new FFmpeg(FFMPEG_PATH);
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput("data/demo.wav")
+                .overrideOutputFiles(true)
+                .addOutput(output.getAbsolutePath())
+                .setFormat("mp3")
+                .done();
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg);
+        executor.createJob(builder).run();
+        System.out.println("Generated mp3 file: " + output.getAbsolutePath());
     }
 
 }
